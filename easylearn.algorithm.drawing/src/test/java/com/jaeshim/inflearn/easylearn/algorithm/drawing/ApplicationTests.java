1cc11201a7f58ab80a0bc8ac31517880ca98aec8
jaeshim <jaeshim@gmarket.com>
2023-09-12 23:26:11 +0900

    20230912

---
+++ easylearn.algorithm.drawing/src/test/java/com/jaeshim/inflearn/easylearn/algorithm/drawing/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeshim.inflearn.easylearn.algorithm.drawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ApplicationTests {
+
+	@Test
+	void test1() {
+		System.out.println("true = " + true);
+	}
+
+}
